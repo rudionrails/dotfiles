1e61df5d093cd25a259bb3c54aa7874bd5f42509
Rudolf Schmidt <me@rudionrails.com>
2024-03-14 00:36:38 +0100

    feat(nvim): Updates neo-tree to be lazu loaded for better startup performance (breaks opening directories)

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -7,28 +7,16 @@ return {
 		"MunifTanjim/nui.nvim",
 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 	},
-	-- cmd = "Neotree",
-	init = function()
-		local map = vim.keymap.set
-
-		map(
-			"n",
-			"<leader>e",
-			"<CMD> Neotree float reveal<CR>",
-			{ silent = true, desc = "File [E]xplorer (NeoTree, floating)" }
-		)
-		map(
-			"n",
-			"<leader>E",
-			"<CMD> Neotree left reveal<CR>",
-			{ silent = true, desc = "File [E]xplorer (NeoTree, left)" }
-		)
-	end,
+	cmd = "Neotree",
+	keys = {
+		{ "<leader>e", "<CMD> Neotree float reveal<CR>", silent = true, desc = "File [E]xplorer (NeoTree, floating)" },
+		{ "<leader>E", "<CMD> Neotree left reveal<CR>", silent = true, desc = "File [E]xplorer (NeoTree, left)" },
+	},
 	config = function()
 		require("neo-tree").setup({
-			filesystem = {
-				hijack_netrw_behavior = "open_current",
-			},
+			-- filesystem = {
+			-- 	hijack_netrw_behavior = "open_current",
+			-- },
 			window = {
 				mappings = {
 					["<space>"] = "none",

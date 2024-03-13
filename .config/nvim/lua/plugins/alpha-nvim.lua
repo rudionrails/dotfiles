4be1ac1c41d2ae29b188360a9f1aeb98d361c5b1
Rudolf Schmidt <me@rudionrails.com>
2024-03-14 00:33:21 +0100

    feat(nvim): Adds startup time to alpha dashboard

--- .config/nvim/lua/plugins/alpha-nvim.lua
+++ .config/nvim/lua/plugins/alpha-nvim.lua
@@ -8,6 +8,8 @@ return {
 		local startify = require("alpha.themes.startify")
 		local config = require("core.config")
 
+		startify.section.header.opts.hl = "AlphaHeader"
+
 		startify.section.top_buttons.val = {
 			-- { type = "text", val = "Shortcuts", opts = { hl = "SpecialComment" } },
 			-- { type = "padding", val = 1 },
@@ -18,17 +20,34 @@ return {
 			-- startify.button("s", " " .. " Restore Session", [[<cmd> lua require("persistence").load() <cr>]]),
 		}
 
+		-- for _, button in ipairs(startify.section.top_buttons) do
+		-- 	button.opts.hl = "AlphaButtons"
+		-- 	button.opts.hl_shortcut = "AlphaShortcut"
+		-- end
+
 		startify.section.bottom_buttons.val = {
-			startify.button("m", config.icons.ui.Package .. " " .. " Mason", "<cmd> Mason <cr>"),
-			startify.button("l", config.icons.ui.Package .. " " .. " Lazy", "<cmd> Lazy <cr>"),
-			startify.button("q", config.icons.ui.Quit .. " " .. " Quit", "<cmd> qa <cr>"),
+			startify.button("m", config.icons.ui.Package .. " " .. " Mason", "<CMD> Mason <CR>"),
+			startify.button("l", config.icons.ui.Package .. " " .. " Lazy", "<CMD> Lazy <CR>"),
+			startify.button("q", config.icons.ui.Quit .. " " .. " Quit", "<CMD> q <CR>"),
 		}
 
-		for _, button in ipairs(startify.section.top_buttons) do
-			button.opts.hl = "AlphaButtons"
-			button.opts.hl_shortcut = "AlphaShortcut"
-		end
+		alpha.setup(startify.config)
 
-		alpha.setup(startify.opts)
+		vim.api.nvim_create_autocmd("User", {
+			once = true,
+			pattern = "LazyVimStarted",
+			callback = function()
+				local stats = require("lazy").stats()
+				local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+				local val = "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms"
+
+				startify.section.footer.val = {
+					{ type = "padding", val = 1 },
+					{ type = "text", opts = { hl = "Comment" }, val = val },
+				}
+
+				pcall(vim.cmd.AlphaRedraw)
+			end,
+		})
 	end,
 }
